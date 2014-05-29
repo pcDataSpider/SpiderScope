d5d76f3ee299538c71d2993570098b57e2c0e761
Alex <o0o.moro.o0o@gmail.com>
2014-05-29 15:04:49 -0500

    Documented all loggint functions

--- logger.py
+++ logger.py
@@ -2,14 +2,17 @@ import wx
 import traceback
 
 VERSION = 0.1   # global version number
+
 ERROR = 1
 WARNING = 2
 INFO = 3
 QUESTION = 4
+
 outFile = None
 printout = False
 fName = "log.txt"
 
+#function logger.write( String ) Write the string int the log as-is
 def write( msg ):
 	try:
 		if printout == True:
@@ -20,6 +23,10 @@ def write( msg ):
 		print "Error writing to log:" + str(e)
 		pass
 
+#function logger.log(String name, String options, Int mode) Add information to the log
+# name = The message to be logged
+# options = additional details about the message
+# mode = specifies the logging level, 1-4, 1 being an error.
 def log(name, options, mode=0):
 	if mode == ERROR:
 		write( "(E)" + name + ": " + str(options) )
@@ -30,6 +37,9 @@ def log(name, options, mode=0):
 	else:
 		write( "(?)" + name + ": " + str(options) )
 	
+#function logger.message(String message, Int mode) Display a message to the user in the form of a dialog box.
+# message = The message to be displayed
+# mode = The logging level, 1-4, 1 being a serious error
 def message(message, mode=0):
 	if mode == ERROR:
 		wx.MessageBox(message, "Error", wx.OK | wx.ICON_ERROR)
@@ -40,6 +50,9 @@ def message(message, mode=0):
 	else:
 		wx.MessageBox(message, "Message", wx.OK | wx.ICON_QUESTION)
 
+#function  logger.ask(String message, Int mode) Ask the user a yes or no question
+# message = the message to be displayed
+# mode = The logging level, 1-4, 1 being a serious error
 def ask(message, mode=4):
 	if mode == ERROR:
 		ret = wx.MessageBox(message, "Error", wx.YES_NO | wx.ICON_ERROR)
@@ -56,6 +69,7 @@ def ask(message, mode=4):
 	else:
 		return False
 
+#function logger.trace() Print a stack trace
 def trace():
 	traceback.print_stack()
 	
